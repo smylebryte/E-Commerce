23de88ffca4732fa6fdfe2a25c9ecd58b4b5368f
Smylebryte <aliciahughes1446@gmail.com>
2021-06-20 20:52:40 -0500

    updated routes

--- models/Product.js
+++ models/Product.js
@@ -1,10 +1,10 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
+const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model { }
+// Initialize Product model (table) by extending off Sequelize Model class
+class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
@@ -14,42 +14,42 @@ Product.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
     product_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
     price: {
       type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
-        isDecimal: true
-      }
+        isDecimal: true,
+      },
     },
     stock: {
       type: DataTypes.INTEGER,
       allowNull: false,
       defaultValue: 10,
       validate: {
-        isNumeric: true
-      }
+        isNumeric: true,
+      },
     },
     category_id: {
       type: DataTypes.INTEGER,
       references: {
-        model: 'category',
-        key: 'id',
-        unique: false
-      }
-    }
+        model: "category",
+        key: "id",
+        unique: false,
+      },
+    },
   },
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'product',
+    modelName: "product",
   }
 );
 
